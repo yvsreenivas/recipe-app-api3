ac2a8025d6fb26afe6cf135120f62e67093ec054
yvsreenivas <yvsreenivas@yahoo.co.in>
2020-12-25 20:16:05 +0530

    After copying model.py from old file

--- app/core/models.py
+++ app/core/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                        PermissionsMixin
+                                       PermissionsMixin
 
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password=None,  **extra_fields):
         """Creates and saves a new user"""
         if not email:
-            raise ValueError("Users must have an email address")
+            raise ValueError('Email address blank')
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -16,17 +17,17 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """Creates and saves super user"""
+        """Creates and saves a new super user"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(self._db)
+        user.save(using=self._db)
 
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that uses email as username """
+    """Custom user model that supports email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
